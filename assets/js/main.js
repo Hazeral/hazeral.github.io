432b62f79aeedc1cced7dacaff138a15af370dd0
Hazeral <zturbin@gmail.com>
2020-10-25 00:24:04 +0100

    init

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function copyID() {
+    let clipboard = document.getElementById("clipboard");
+    let notification = document.getElementById("notification");
+
+    clipboard.value = "239381059551100931";
+
+    clipboard.select();
+    clipboard.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    notification.style.opacity = 100;
+    setInterval(() => notification.style.opacity = 0, 2000);
+}
